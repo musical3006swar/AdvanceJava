b52dfffe0f4d78a0d1158544321b253ce430286a
Swar <4al22cs176@gmail.com>
2025-05-22 19:44:13 +0530

    Update and rename searchcolor_red.java to Searchcolor_red.java

--- Exp1-ListInterface/Searchcolor_red.java
+++ Exp1-ListInterface/Searchcolor_red.java
@@ -1,7 +1,7 @@
 //Q1b-1: Write a java program for getting different colors through ArrayList interface and search whether the color "Red" is available or not
 package ListOperation;
 import java.util.*;
-public class searchcolor_red{
+public class Searchcolor_red{
     public static void main(String[] args) {
         ArrayList<String> colors = new ArrayList<>(Arrays.asList("Red", "Green", "Blue", "Yellow"));
         System.out.println("Colors: " + colors);
